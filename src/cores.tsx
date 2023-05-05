7ec99ef258f1e96ec557af5be798e455ae2f331e
Débora Carvalho <debora.sesv@gmail.com>
2023-05-05 09:19:45 -0300

    Desafio Styleguide cores resolvido

---
+++ src/cores.tsx
@@ -0,0 +1,36 @@
+const coresBrand = {
+  primary: {
+    Dark: "#031F3D",
+    Medium: "#0C4888",
+  },
+  ctaMedium: {
+    cta: "#29D6E7",
+  },
+  acessory: {
+    backgroundSecundario: "#0C4888",
+    backgroundBanner: "#28ADBA",
+  },
+
+};
+const neutral = {
+  white: "#FFFFFF",
+  neutralXLight: "#FDFDFD",
+  neutralLight: "#C5C5C5",
+  neutralMedium: "#908E8E",
+  neutralDark: "#3A3335",
+  neutralXDark: "#484848",
+};
+
+const feedback = {
+  FeedbackSuccess: "#2AC084",
+  FeedbackError: "#EB5E55",
+};
+
+export { }
+
+
+
+
+
+
+
