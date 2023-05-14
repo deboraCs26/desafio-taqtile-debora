d923393b4ed6ee2a221a6c38584166fa11e25efb
Débora Carvalho <debora.sesv@gmail.com>
2023-05-14 18:34:53 -0300

    Corrigir conflitos de mesclagem

--- src/style/button/style-button.ts
+++ src/style/button/style-button.ts
@@ -3,7 +3,6 @@ export const styleButton = {
     radius: "4px",
     borderRadius: "4px",
     padding: "16px",
-<<<<<<< HEAD
     width: "143px ",
     left: "20px",
     justifyContent: "Center",
@@ -15,14 +14,6 @@ export const styleButton = {
     height: {
     default:"48px",
     defaultCompact:"40px"
-  
-=======
-    Width: "148px",
-    height: "48px"
-  },
-  secondary: {
-    
->>>>>>> 55b1634416994dec6057e110ed1b92013731110d
   },
 };
 
