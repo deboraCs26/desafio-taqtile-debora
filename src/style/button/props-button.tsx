3b8575e01b7f8d45b3f4dc5a88f000610857d1ab
debora carvalho <debora.sesv@gmail.com>
2023-05-17 02:01:46 -0300

    button stylegruide challenge finished

--- src/style/button/props-button.tsx
+++ src/style/button/props-button.tsx
@@ -4,62 +4,39 @@ import { colors } from "../colors";
 import { components } from "../typography/typography";
 import { styleButton } from "./style-button";
 
+
 interface ButtonProps {
-  variant: "primary" | "secondary" | "cta";
+  variant: 'primary' | 'secondary' | 'cta';
   children: React.ReactNode;
   onClick?: () => void;
   disabled?: boolean;
-  alt?: string;
   icon?: React.ReactNode;
-  compact?: boolean;
+  height?: "default" | "defaultCompact";
   style?: React.CSSProperties;
+};
 
-}
-
-export const Button = ({ variant, children, icon, disabled = false, onClick, compact, alt }: ButtonProps) => {
-
-  const buttonConstantStyle = {
-    fontFamily: components.family.primary,
-    fontSize: components.fontSize.large,
-    fontWeight: components.fontWeight.bold,
-    minWidth: styleButton.style.minWidth,
-    alignItems: styleButton.style.alignItems,
-    borderRadius: styleButton.style.borderRadius,
-    borderColor: colors.accessory.banner,
-    cursor: "pointer",
-  };
-
-  let backgroundColor, color, border;
-  if (variant === "primary") {
-    backgroundColor = disabled
-      ? colors.neutral.neutralLight
-      : colors.brand.primaryXDark;
-    color = colors.neutral.white;
-    border = styleButton.style.border
-  } else if (variant === "secondary") {
-    backgroundColor = colors.neutral.white;
-    color = colors.accessory.banner;
-    border = `1px solid ${colors.accessory.banner}`;
-  } else if (variant === "cta") {
-    backgroundColor = colors.cta;
-    color = colors.neutral.white;
-    border = styleButton.style.border
-  }
-
-  const styleDoButton = {
-    ...buttonConstantStyle,
-    backgroundColor,
-    color,
-    border,
-    height: compact ? "40px" : "48px",
-  };
-
+export const Button = ({ variant, children, icon, disabled = false, onClick, height }: ButtonProps) => {
   return (
     <button
+      className={`button ${height}`}
       disabled={disabled}
       onClick={onClick}
-      style={styleDoButton}
+      style={{
+        fontFamily: components.family.primary,
+        fontSize: components.fontSize.large,
+        fontWeight: components.fontWeight.bold,
+        width: styleButton.style.minWidth,
+        alignItems: styleButton.style.alignItems,
+        borderRadius: styleButton.style.borderRadius,
+        borderColor: colors.accessory.banner,
+        background: variant === "secondary" ? colors.neutral.white : variant === "cta" ? colors.cta : variant === "primary" ? disabled ? colors.neutral.neutralLight : colors.brand.primary.primayXDark : "",
+        color: variant === "primary" ? colors.neutral.white : variant === "secondary" ? colors.accessory.banner : variant === "cta" ? colors.neutral.white : "",
+        border: variant === "secondary" ? `1px solid ${colors.accessory.banner}` : "none",
+        cursor: "pointer",
+      }}
+
     >
+      {icon && <span>{icon}</span>}
       {children}
     </button>
   );
