b99e37e0ad4ed78f2a85acc08d6fb463702e2616
debora carvalho <debora.sesv@gmail.com>
2023-05-28 14:41:39 -0300

    card correction

--- src/style/Info-Tile/infoTile.tsx
+++ src/style/Info-Tile/infoTile.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import './infoTile.css';
 import { colors } from '../colors';
-import {components} from '../typography/typography'
+import { components } from '../typography/typography';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCar } from '@fortawesome/free-solid-svg-icons';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { H4 } from '../typography/h4';
 
 interface InfoTileProps {
-  children?: React.ReactNode;
   icon?: IconDefinition;
   text?: string;
 }
-export const styleInfoTile = {
-  fontFamily: components.family.primary,
-  fontSize: components.fontSize.medium,
-  fontWeigth: components.fontWeight.semiBold,
-  color: colors.neutral.neutralXDark,
-  background: colors.neutral.neutralXLight,
-  border: `1.5px solid ${colors.neutral.neutralXDark}`
+
+export const styleIcon ={
+fontSize: components.fontSize.XLarge,
+color: colors.neutral.neutralXDark,
 }
-export const InfoTile = ({ children, icon, text }: InfoTileProps) => {
+
+export const InfoTile = ({text }: InfoTileProps) => {
   return (
-      <div className="style-infoTile" style={styleInfoTile}>
-        <FontAwesomeIcon icon={icon || faCar} style={{ color: colors.neutral.neutralXDark }} />
-        <h4 className="info-tile-text">{text}</h4>
+      <div className="style-info-tile">
+        <FontAwesomeIcon icon={faCar} style={ styleIcon}/>
+        <H4>{text}</H4>
       </div>
   );
 };
