3dc1c4bcf71e47db274c09973664e3e82061f37b
debora carvalho <debora.sesv@gmail.com>
2023-05-28 13:01:27 -0300

    styleguide form correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,105 @@
+import React, { useState } from 'react';
 import './App.css';
-import { H1 } from './style/typography/h1';
 import { Form } from './style/form/form';
-
+import { Display } from './style/typography/display';
+import { H1 } from './style/typography/h1';
+import { H2 } from './style/typography/h2';
+import { H3 } from './style/typography/h3';
+import { H4 } from './style/typography/h4';
+import { Body1 } from './style/typography/body1';
+import { Body2 } from './style/typography/body2';
+import { DT } from './style/typography/dt-dd';
+import { Label } from './style/typography/LargeLabel';
+import { Caption } from './style/typography/caption';
+import { Price } from './style/typography/medium-price';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 export default function App() {
+  const [senhaInvalida, setSenhaInvalida] = useState(false);
+
+  const handleFormSubmit = () => {
+    if (senhaInvalida) {
+      setSenhaInvalida(true);
+    } else {
+      setSenhaInvalida(false);
+    }
+  };
   return (
     <div>
       <H1>Formulario</H1>
+      <Form label='Nome'/>
+        <Form label='Email'/>
+        <Form caption="Senha inválida" password label="Senha" icon={faExclamationTriangle} error={true} />
+        <button onClick={handleFormSubmit}>Submit</button>
+        <Display>
+        display
+      </Display>
+      
+      <H2>
+        Headline 2
+      </H2>
 
-      <div>
-        <Form label='Nome'></Form>
-        <Form label='Email'></Form>
-        <Form caption="Caption" label='Senha'></Form>
-      </div>
+      <H3>Headline 3</H3>
+
+      <H4>
+        Headline 4
+      </H4>
+
+      <Body1>
+        body1
+      </Body1>
+
+      <Body2 weight="regular">
+        body2
+      </Body2>
+
+      <Body2 weight="bold">
+        body2
+      </Body2>
+
+      <Body2 weight="regular">
+        body2
+      </Body2>
+
+      <DT weight="regular" >
+        Definition of the Term
+      </DT>
+
+      <DT weight="bold">
+        Definition of the Term
+      </DT>
+
+      <Label weight="bold" color="dark" size="large">
+        Label large bold
+      </Label>
+
+      <Label weight="semiBold" color="dark" size="large">
+        Label large Semibold
+      </Label>
+
+      <Label weight="regular" color="dark" size="large">
+        Label large regular
+      </Label>
+
+      <Label weight="regular" color="white" size="medium">
+        Label large
+      </Label>
+
+      <Caption>
+        Caption
+      </Caption>
+
+      <Price size='xxxLarge' weight='bold'>
+        Big Price
+      </Price>
+
+      <Price size='XLarge' weight='bold'>
+        Medium Price
+      </Price>
+
+      <Price size='medium' weight='regular'>
+        Small Price
+      </Price>
 
     </div>
-
   );
 };
--- src/style/form/form.tsx
+++ src/style/form/form.tsx
@@ -1,42 +1,47 @@
 import React, { useState } from 'react';
 import './style-form.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { colors } from '../colors';
+import { components } from '../typography/typography';
 import { Label } from '../typography/LargeLabel';
-
 interface FormProps {
+  error?: boolean;
   caption?: string;
   label?: string;
+  iconError?: IconDefinition;
   icon?: IconDefinition;
+  password?: boolean;
 }
-export const Form = ({ caption, label, icon }: FormProps) => {
-  const [focused, setFocused] = useState(false);
-  const [error, setError] = useState("");
 
-  let border, borderColor;
+export const styleError = {
+  fontFamily: components.family.primary,
+  color: colors.neutral.neutralDark,
+}
+
+export const Form = ({error, label, icon, password, caption, iconError}: FormProps) => {
+  const [focused, setFocused] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  let border;
 
   if (focused) {
-    border = `1px solid ${colors.neutral.neutralLight}`;
+    border = `2px solid ${colors.neutral.neutralLight}`;
   } else if (error) {
     border = `1px solid ${colors.feedback.feedbackError}`;
-  } else {
+  } else if (inputValue) {
     border = `1px solid ${colors.neutral.neutralLight}`;
-  };
+  } else {
+    border= `1px solid`;
+  }
 
   const inputStyle = {
     border,
-    borderColor,
-  };
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.target.value;
-    if (inputValue) {
-      setError("");
-    } else {
-      setError("Este campo é obrigatório.");
-    }
   };
 
   const handleInputFocus = () => {
@@ -48,34 +53,26 @@ export const Form = ({ caption, label, icon }: FormProps) => {
   };
 
   return (
-    <div>
-      {!!icon && <FontAwesomeIcon icon={icon} />}
-      <div className="input-container form">
-        <Label color="dark">{label}</Label>
+    <div className='form-container'>
+      <div className='input-container'>
+        <Label color='dark'>{label}</Label>
         <input
-          className="input-style"
+          className='input-style'
           style={inputStyle}
-          type={label === "Senha" ? "password" : "text"}
+          type={password ? 'password' : 'text'}
           placeholder={label}
           onChange={handleInputChange}
           onFocus={handleInputFocus}
           onBlur={handleInputBlur}
         />
         {error && (
-          <span style={{ color: colors.feedback.feedbackError }}>
-            {error}
-          </span>
+          <span style={styleError}>
+            {!!icon && <FontAwesomeIcon icon={faExclamationTriangle}/>}
+            {!!iconError && <FontAwesomeIcon icon={iconError} />}
+            {caption}
+          </span> 
         )}
       </div>
-      {caption && (
-        <div className="icon-caption-container">
-          <FontAwesomeIcon
-            icon={faExclamationTriangle}
-            style={{ margin: "10px", color: colors.feedback.feedbackError }}
-          />
-          <span style={{ color: colors.feedback.feedbackError }}>{caption}</span>
-        </div>
-      )}
     </div>
   );
 };
