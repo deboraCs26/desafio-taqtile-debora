672e3383156bfd79dd852d13cdc7145002afa684
Débora <119463609+deboraCs26@users.noreply.github.com>
2023-06-29 00:58:25 -0300

    rebase

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import './App.css';
 import { Hero } from './style/hero-section';
 import { Banner } from './style/banner';
 import { VisitingCard } from './style/visiting-card/visiting-card';
-import { Display } from './style/typography/display';
 import { H2 } from './style/typography/h2';
 import { CardCategory } from './style/card-categorias-de-imoveis/card-category';
 import { CategoryCarousel } from './style/category-carousel/carousel-section';
 import imageCategory from './style/card-categorias-de-imoveis/cards/image-category.svg';
-import imgVisiting from './style/card-to-schedule-visits/img/Img.png'
+import imgVisiting from './style/visiting-card/img/Img.png'
 
 export default function App() {
 
@@ -29,42 +28,6 @@ export default function App() {
           title="Com Pricina"
           description="723,457 Imóveis"
         />
-      <div>
-        <VisitingCard
-          bigPrice="R$ 250.000"
-          image={imgVisiting}
-          locationText="Condomínio"
-          iptuText='IPTU'
-          price='R$1450,00'
-          condominiumPrice='R$450,00'
-          description=' Jonathan Marques'
-          captions={['Imobilar corretora imobiliária', 'CRECI-SP J22.004']}
-          icon
-        />
-
-        <VisitingCard
-          bigPrice="R$ 2900.00"
-          image={imgVisiting}
-          locationText="Condomínio"
-          iptuText="IPTU"
-          price="R$ 100,00"
-          condominiumPrice="R$ 450,00"
-          description="Jonathan Marques"
-          captions={['Imobilar corretora imobiliária', 'CRECI-SP J22.004']}
-          icon
-          iconColor='red'
-          serviceFeeLabel="Taxas Extras"
-          serviceFeeValue="R$ 100,00"
-          rentLabel="Aluguel Mensal"
-          rentValue="R$ 2.250,00"
-          value='Total'
-          amount='2900,00'
-        />
-
-      </div>
-      <Display>
-        display
-      </Display>
 
         <CardCategory
           image={imageCategory}
@@ -98,6 +61,40 @@ export default function App() {
         />
 
       </CategoryCarousel>
+      <div>
+        <VisitingCard
+          bigPrice="R$ 250.000"
+          image={imgVisiting}
+          locationText="Condomínio"
+          iptuText='IPTU'
+          price='R$1450,00'
+          condominiumPrice='R$450,00'
+          description=' Jonathan Marques'
+          captions={['Imobilar corretora imobiliária', 'CRECI-SP J22.004']}
+          icon
+        />
+
+        <VisitingCard
+          bigPrice="R$ 2900.00"
+          image={imgVisiting}
+          locationText="Condomínio"
+          iptuText="IPTU"
+          price="R$ 100,00"
+          condominiumPrice="R$ 450,00"
+          description="Jonathan Marques"
+          captions={['Imobilar corretora imobiliária', 'CRECI-SP J22.004']}
+          icon
+          iconColor='red'
+          serviceFeeLabel="Taxas Extras"
+          serviceFeeValue="R$ 100,00"
+          rentLabel="Aluguel Mensal"
+          rentValue="R$ 2.250,00"
+          value='Total'
+          amount='2900,00'
+        />
+
+      </div>
     </div>
+    
   );
 };
