e2b27fdbc4a1e84bccc2069ae106a71f577cd8f1
Débora <119463609+deboraCs26@users.noreply.github.com>
2023-07-01 22:25:07 -0300

    finished footer

--- src/style/footer/index.tsx
+++ src/style/footer/index.tsx
@@ -1,18 +1,62 @@
 import React from "react";
 import './style.css';
-import iconFooter from './image/Iconsax/Linear/icon-footer.svg'
-import imageImobilar from './image/Imobilar Corretora Imobiliária.svg'
+import logo from './image/LOGO.svg'
+import { FooterBlock } from "./footer";
+import { Separator } from "../separator/separator";
 
-export const  Footer = () => {
-  return (
-    <section>
-      <div className="footer-container" >
-        <img src={iconFooter} alt="icon de uma casa" className="icon-footer"/>
-        <img src={imageImobilar} alt="logo"/>
+export const Footer = () => (
+
+  <footer>
+    <div  className="footer-container">
+      <div className="logo-container">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="itens-container">
+      <div>
+        <FooterBlock
+          title="Comprar"
+          items={['Casas', 'Apartamentos', 'Lançamentos', 'Melhores negócios']}
+        />
+        <Separator size="large" />
+        <Separator size="large" />
+        <Separator size="large" />
         <div>
-       
+          <FooterBlock
+            title="Alugar"
+            items={['Casas', 'Apartamentos', 'Lançamentos', 'Melhores negócios']}
+          />
         </div>
       </div>
-    </section>
-  );
-}; 
+
+      <div>
+        <FooterBlock
+          title="Termos & Privacidade"
+          items={['Política de cookies', 'Termos de uso', 'Política de privacidade']}
+        />
+        <Separator size="large" />
+        <Separator size="large" />
+        <Separator size="large" />
+        <Separator size="large" />
+        <div>
+          <FooterBlock
+            title="Institucional"
+            items={['Sobre nós', 'Contato', 'Trabalhe conosco']}
+          />
+        </div>
+      </div>
+
+      <div>
+        <FooterBlock
+          title="Informações"
+          items={['Blog', 'Guia', 'FAQ', 'Canal de ajuda']}
+        />
+      </div>
+      </div>
+     
+    </div>
+    <Separator size="small"/>
+    <div className="rodape">
+      <FooterBlock items={['©2022 Estatery. Todos os direitos estão reservados']} />
+    </div>
+  </footer>
+);
