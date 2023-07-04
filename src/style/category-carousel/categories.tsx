e37a36a18691e97dff32e845fe0db6865d56a527
Débora <119463609+deboraCs26@users.noreply.github.com>
2023-07-04 01:40:37 -0300

    correction of carousel section of categories using mocks

---
+++ src/style/category-carousel/categories.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './carousel-section.css';
+import { CategoryCarousel } from './carousel-section';
+import { H2 } from '../typography/h2';
+
+interface CarouselSectionProps {
+  title: string;
+}
+
+export const CarouselSection = ({ title }: CarouselSectionProps) => {
+  const categories = [
+    {
+      name: 'Com piscina',
+      imageUrl: 'https://storage.cloud.google.com/instituto-taqtile/desafio/piscina.png',
+      numberOfProperties: 500
+    },
+    {
+      name: 'Com piscina',
+      imageUrl: 'https://storage.cloud.google.com/instituto-taqtile/desafio/piscina.png',
+      numberOfProperties: 500
+    },
+    {
+      name: 'Com piscina',
+      imageUrl: 'https://storage.cloud.google.com/instituto-taqtile/desafio/piscina.png',
+      numberOfProperties: 500
+    },
+    {
+      name: 'Com piscina',
+      imageUrl: 'https://storage.cloud.google.com/instituto-taqtile/desafio/piscina.png',
+      numberOfProperties: 500
+    },
+    {
+      name: 'Com piscina',
+      imageUrl: 'https://storage.cloud.google.com/instituto-taqtile/desafio/piscina.png',
+      numberOfProperties: 500
+    },
+    {
+      name: 'Com piscina',
+      imageUrl: 'https://storage.cloud.google.com/instituto-taqtile/desafio/piscina.png',
+      numberOfProperties: 500
+    },
+    {
+      name: 'Com piscina',
+      imageUrl: 'https://storage.cloud.google.com/instituto-taqtile/desafio/piscina.png',
+      numberOfProperties: 600
+    }
+  ];
+
+  return (
+    <div>
+      <H2>{title}</H2>
+      <CategoryCarousel>
+        {categories.map((category, index) => (
+          <div key={index}>
+            <img src={category.imageUrl} alt='' />
+          </div>
+        ))}
+      </CategoryCarousel>
+    </div>
+  );
+};
