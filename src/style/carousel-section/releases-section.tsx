e53a07993aa9624ce3eea85c290ff3947888fa8b
Débora <119463609+deboraCs26@users.noreply.github.com>
2023-07-07 03:25:32 -0300

    corrections made

--- src/style/carousel-section/releases-section.tsx
+++ src/style/carousel-section/releases-section.tsx
@@ -7,31 +7,32 @@ interface CarouselSectionProps {
   title: string;
 }
 
-export const ReleasesSection = ({ title }: CarouselSectionProps) => {
-  const categories = [
-    {
-      name: 'Seção de lançamentos',
-      imageUrl: 'https://user-images.githubusercontent.com/119463609/251038153-2e961418-2eab-484d-a69e-676d7db4b325.png',
-      numberOfProperties: 500
-    },
-    {
-      name: 'Seção de lançamentos',
-      imageUrl: 'https://user-images.githubusercontent.com/119463609/251038153-2e961418-2eab-484d-a69e-676d7db4b325.png',
-      numberOfProperties: 500
-    },
-    {
-      name: 'Seção de lançamentos',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGL321o66Mn0NEtfHO3kC-enQC2SogHd9oFjhu3wuFFDGjJPCLhK7Yu6gl93NwyeRofxY&usqp=CAU',
-      numberOfProperties: 600
-    },
-  ];
+const categories = [
+  {
+    name: 'Seção de lançamentos',
+    imageUrl: 'https://user-images.githubusercontent.com/119463609/251038153-2e961418-2eab-484d-a69e-676d7db4b325.png',
+    numberOfProperties: 500
+  },
+  {
+    name: 'Seção de lançamentos',
+    imageUrl: 'https://user-images.githubusercontent.com/119463609/251038153-2e961418-2eab-484d-a69e-676d7db4b325.png',
+    numberOfProperties: 500
+  },
+  {
+    name: 'Seção de lançamentos',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGL321o66Mn0NEtfHO3kC-enQC2SogHd9oFjhu3wuFFDGjJPCLhK7Yu6gl93NwyeRofxY&usqp=CAU',
+    numberOfProperties: 600
+  },
+];
 
+export const ReleasesSection = ({ title }: CarouselSectionProps) => {
+ 
   return (
     <div>
       <H2 color='Xdark'>{title}</H2>
       <CarouselSection>
-        {categories.map((category, index) => (
-          <div key={index}>
+        {categories.map((category, id) => (
+          <div key={id}>
             <img src={category.imageUrl} alt='' />
           </div>
         ))}
