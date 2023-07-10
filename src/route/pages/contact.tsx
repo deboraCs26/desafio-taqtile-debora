63ae7ecdebf9b3de88b57fc034de2141e1c32fc2
Débora <119463609+deboraCs26@users.noreply.github.com>
2023-07-10 02:43:24 -0300

    navigation integration task done

---
+++ src/route/pages/contact.tsx
@@ -0,0 +1,11 @@
+import { H2 } from "../../style/typography/h2";
+import { Menu } from "../../style/header";
+
+export function Contact() {
+  return (
+    <>
+      <Menu />
+      <H2 color="XDark">Pagina de Contato</H2>
+    </>
+  );
+};
---
+++ src/route/pages/who-we-are.tsx
@@ -0,0 +1,11 @@
+import { H2 } from "../../style/typography/h2";
+import { Menu } from "../../style/header";
+
+export function WhoAre() {
+  return (
+    <>
+      <Menu />
+      <H2>Pagina quem somos</H2>
+    </>
+  );
+};
