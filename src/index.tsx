c01e3eefdcf2ed05eff3162e88a32f88a754f114
Débora <119463609+deboraCs26@users.noreply.github.com>
2023-07-09 23:03:32 -0300

    integration create route configuration done

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from './route/home';
 
 const client = new ApolloClient({
   uri: 'https://taqtile-challenge-api-sjz6wnaoia-uc.a.run.app/graphql',
   cache: new InMemoryCache(),
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <ApolloProvider client={client}>
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   </ApolloProvider>
 );
