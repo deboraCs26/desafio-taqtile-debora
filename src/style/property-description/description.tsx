660cc04e324d60b3f2b93fa403bb7976f274fea6
Débora <119463609+deboraCs26@users.noreply.github.com>
2023-07-17 03:23:26 -0300

    adding card

--- src/style/property-description/description.tsx
+++ src/style/property-description/description.tsx
@@ -3,6 +3,7 @@ import './description.css';
 import { H1 } from "../typography/h1";
 import { Body2 } from "../typography/body2";
 import location from "./icon/location.png"
+import imageDoCard from "./image-do-card/Img.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBath, faCar } from '@fortawesome/free-solid-svg-icons';
 import { faSwimmingPool, faRuler, faBed } from '@fortawesome/free-solid-svg-icons';
@@ -11,6 +12,7 @@ import { H4 } from "../typography/h4";
 import { H2 } from "../typography/h2";
 import { colors } from "../colors";
 import { Separator } from "../separator/separator";
+import { VisitingCard } from "../visiting-card/visiting-card";
 
 interface PropertyDescriptionProps {
   bathrooms?: number;
@@ -22,7 +24,7 @@ interface PropertyDescriptionProps {
 
 const iconStyle = {
   color: colors.neutral.neutralXDark
-}
+};
 
 export const Description = ({ bathrooms, bedrooms, area, pool, vacancy }: PropertyDescriptionProps) => {
 
@@ -36,37 +38,55 @@ export const Description = ({ bathrooms, bedrooms, area, pool, vacancy }: Proper
 
   return (
     <div className="description-container">
-      <div className="title-properties">
-        <H1 color="XDark">Casa à venda em Osasco,  bairro Bonfim, melhor da região!</H1>
-      </div>
-
-      <div className="andress">
-        <img src={location} alt="endereço do imóvel" />
-        <Separator size="small" horizontal />
-        <Body2 color="XDark">Avenida das Nações Unidas 3239, Complemento 245B, Bonfim, Osasco</Body2>
-      </div>
-
-      <Separator size="XLarge" />
-      <ContainerCard>
-        <div className="card-description">
-          {description.map((item, id) => (
-            <div className="item" key={id}>
-              {item.icon}
-              <H4 color="XDark" weight="semiBold">{item.title}</H4>
-            </div>
-          ))}
+      <div className="text-description">
+        <div className="title-properties">
+          <H1 color="XDark">Casa à venda em Osasco,  bairro Bonfim, melhor da região!</H1>
         </div>
-      </ContainerCard>
-    
-      <Separator size="XLarge" />
-      <div className="description">
-        <H2 color="XDark">Descrição</H2>
 
-        <Body2 color="XDark">
-          A casa se insere em um clima de paz, de natureza. A decoração acompanha a magia desse microclima ecológico que encanta a adultos e crianças. A casa foi construída em madeira nobre. Decorada com lustres temáticos e estruturas que dão vida ao lugar. Ideal para todos que busquem descansar ou trabalhar e desejam morar na melhor região de Osasco- SP.
-        </Body2>
+        <div className="andress">
+          <img src={location} alt="endereço do imóvel" />
+          <Separator size="small" horizontal />
+          <Body2 color="XDark">Avenida das Nações Unidas 3239, Complemento 245B, Bonfim, Osasco</Body2>
+        </div>
+
+        <Separator size="XLarge" />
+        <ContainerCard>
+          <div className="card-description">
+            {description.map((item, id) => (
+              <div className="item" key={id}>
+                {item.icon}
+                <H4 color="XDark" weight="semiBold">{item.title}</H4>
+              </div>
+            ))}
+          </div>
+        </ContainerCard>
+
+        <Separator size="XLarge" />
+        <div className="description" >
+          <H2 color="XDark">Descrição</H2>
+
+          <Body2 color="XDark">
+            A casa se insere em um clima de paz, de natureza. A decoração acompanha a magia desse microclima ecológico que encanta a adultos e crianças. A casa foi construída em madeira nobre. Decorada com lustres temáticos e estruturas que dão vida ao lugar. Ideal para todos que busquem descansar ou trabalhar e desejam morar na melhor região de Osasco- SP.
+          </Body2>
+
+
+        </div>
       </div>
+
+      <div className="visiting-card">
+        <VisitingCard
+          image={imageDoCard}
+          bigPrice="R$250.000"
+          condominiumPrice="Condomínio"
+          locationText="1450,00"
+          iptuText="IPTU"
+          price="450,00"
+          description="Jonathan Marques"
+          captions={["Imobilar Corretora Imobiliária", "CRECI-SP J22.004"]}
+          icon
+        />
+      </div>
+
     </div>
   );
-
 };
