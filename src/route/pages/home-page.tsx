23def19d3c2cda75c1e706f1a4703cf67edbc339
Débora <119463609+deboraCs26@users.noreply.github.com>
2023-07-20 20:35:20 -0300

    adding task feature/building-items

--- src/route/pages/home-page.tsx
+++ src/route/pages/home-page.tsx
@@ -1,6 +1,11 @@
 import { Menu } from "../../style/header";
 import { PhotoProperty } from "../../style/photo-section/property-photos";
 import { PropertyCard } from "../../style/property-description/property-card";
+<<<<<<< HEAD
+=======
+import { PropertyItems } from "../../style/property-items/items";
+import { Separator } from "../../style/separator/separator";
+>>>>>>> e8943d5 (adding task feature/building-items)
 
 export function HomePage() {
   return (
@@ -10,6 +15,12 @@ export function HomePage() {
       <PhotoProperty />
 
       <PropertyCard />
+<<<<<<< HEAD
+=======
+  
+      <Separator size="XLarge" />
+      <PropertyItems />
+>>>>>>> e8943d5 (adding task feature/building-items)
     </>
   );
 };
