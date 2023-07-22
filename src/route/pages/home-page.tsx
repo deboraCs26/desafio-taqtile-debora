b716395c2618f55f522df2f1354a2f4c0a06f5c8
Débora <119463609+deboraCs26@users.noreply.github.com>
2023-07-21 21:20:00 -0300

    proximity section task

--- src/route/pages/home-page.tsx
+++ src/route/pages/home-page.tsx
@@ -3,9 +3,12 @@ import { Menu } from "../../style/header";
 import { PhotoProperty } from "../../style/photo-section/property-photos";
 import { PropertyCard } from "../../style/property-description/property-card";
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import { PropertyItems } from "../../style/property-items/items";
 >>>>>>> e8943d5 (adding task feature/building-items)
+=======
+>>>>>>> 6a77155 (proximity section task)
 import { Separator } from "../../style/separator/separator";
 
 export function HomePage() {
@@ -21,7 +24,10 @@ export function HomePage() {
       <Separator size="XLarge" />
       
       <Footer />
+<<<<<<< HEAD
 
+=======
+>>>>>>> 6a77155 (proximity section task)
     </>
   );
 };
---
+++ src/style/proximity-section/proximity.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { colors } from "../colors";
+import { ProximitySection } from "./proximity-section";
+import { faUtensils, faLeaf, faGraduationCap, faHospital, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+export const Proximity = () => {
+
+  const proximityIcon = [
+    { icon: faUtensils, title: "Restaurantes bem avaliados", text: "Cabanã Osasco,  Salva Burguer, Jin Chef e O Bom do Baião.", color: colors.neutral.neutralLight },
+    { icon: faLeaf, title: "Parques e áreas verdes", text: "2° Floresta Urbana de Osasco, Parque Chico Mendes e Praça Laurindo de Camargo.", color: colors.neutral.neutralLight },
+    { icon: faGraduationCap, title: "Escolas e colégios", text: "E.E. Profº Francisco Casabona, ETEC Professor André Bogasian e Colégio Padre Anchieta.", color: colors.neutral.neutralLight },
+    { icon: faGraduationCap, title: "Faculdades", text: "Universidade Estácio - Polo Osasco, Universidade Anhembi Morumbi e FNJ - Faculdade Nove de Julho.", color: colors.neutral.neutralLight },
+    { icon: faHospital, title: "Hospitais", text: "Hospital Nossa Senhora de Fátima e Hospital Geral de Carapicuíba.", color: colors.neutral.neutralLight },
+    { icon: faShoppingCart, title: "Mercados", text: "Atacadão, Mercado Moretti Bittencourt e Mercado Municipal de Osasco.", color: colors.neutral.neutralLight },
+
+  ];
+
+  return (
+    <div className="proximity-container">
+      <div className="proximity-list">
+        <ProximitySection title="Proximidades" />
+      </div>
+      <ProximitySection itemsNeutral={proximityIcon} />
+    </div>
+  );
+};
